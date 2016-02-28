c7f6d448c797961adb3127a2b659fac29248557b
Ram Mandal <ram.cp18113@gmail.com>
2016-02-28 13:04:43 +0545

    play video,invoke images/cartoons  on gallery items click

---
+++ app/src/main/java/com/bidhee/nagariknews/Utils/ToggleRefresh.java
@@ -0,0 +1,7 @@
+package com.bidhee.nagariknews.Utils;
+
+/**
+ * Created by ronem on 2/26/16.
+ */
+public class ToggleRefresh {
+}
---
+++ app/src/main/java/com/bidhee/nagariknews/views/activities/YoutubePlayerActivity.java
@@ -0,0 +1,7 @@
+package com.bidhee.nagariknews.views.activities;
+
+/**
+ * Created by ronem on 2/26/16.
+ */
+public class YoutubePlayerActivity {
+}
